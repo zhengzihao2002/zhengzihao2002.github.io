569b968313bcbfcae315a3954d9559c716dc92d0
Zihao Zheng <alvinzheng2002@icloud.com>
2023-03-06 20:12:04 -0500

    Add files via upload

--- JS/index.js
+++ JS/index.js
@@ -4,38 +4,66 @@ document.addEventListener('DOMContentLoaded', () => {
     const logo = header.querySelector('.logo>h1');
     const options = header.querySelectorAll('nav ul li a');
 
-    // add an event listener to listen for scroll events
-    window.addEventListener('scroll', () => {
-    // check if we have scrolled more than 1px from the top
-    if (window.scrollY > 1) {
-        // if we have, decrease the height of the header to 80px
-        header.style.height = '60px';
-        logo.style.fontSize = '1.5em';
-        for(let i=0;i<options.length;i++){
-            options[i].style.fontSize='1.15em';
-        }
+    function smaller(){
+        // check if we have scrolled more than 1px from the top
+        if (window.scrollY > 1) {
+            // if we have, decrease the height of the header to 80px
+            header.style.height = '60px';
+            logo.style.fontSize = '1.5em';
+            for(let i=0;i<options.length;i++){
+                options[i].style.fontSize='1.15em';
+            }
 
-        header.style.backgroundColor=  "#212529";
-        for(let i=0;i<options.length-1;i++){
-            options[i].style.color = "white";
-        }
-        
-        
-    }else {
-        // if we haven't, set the height of the header back to its original value
-        header.style.height = '';
-        logo.style.fontSize = '';
-        for(let i=0;i<options.length;i++){
-            options[i].style.fontSize='';
-        }
+            header.style.backgroundColor=  "#212529";
+            for(let i=0;i<options.length-1;i++){
+                options[i].style.color = "white";
+            }
+            
+            
+        }else {
+            // if we haven't, set the height of the header back to its original value
+            header.style.height = '';
+            logo.style.fontSize = '';
+            for(let i=0;i<options.length;i++){
+                options[i].style.fontSize='';
+            }
 
 
-        header.style.backgroundColor=  "";
-        for(let i=0;i<options.length-1;i++){
-            options[i].style.color = "";
-        }
+            header.style.backgroundColor=  "";
+            for(let i=0;i<options.length-1;i++){
+                options[i].style.color = "";
+            }
 
+        }
     }
-    });
+
+    // add an event listener to listen for scroll events
+    window.addEventListener('scroll', smaller);
+
+
+    
+
+    function handleScreenResize() {
+        const screenHeight = window.innerHeight;
+        
+        if (screenHeight <= 750) {
+            header.style.height = '60px';
+            logo.style.fontSize = '1.5em';
+            for(let i=0;i<options.length;i++){
+                options[i].style.fontSize='1.15em';
+            }
+
+            header.style.backgroundColor=  "#212529";
+            for(let i=0;i<options.length-1;i++){
+                options[i].style.color = "white";
+            }
+            window.removeEventListener('scroll', smaller);
+        }else{
+            window.addEventListener('scroll', smaller);
+
+        }
+    }
+      
+    window.addEventListener('resize', handleScreenResize);
 });
 
